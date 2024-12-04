40b7ad7614ffc679a9098d03e7049ddb6d435115
MR LUTHER <lutherofficial009@gmail.com>
2024-12-04 16:44:31 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,77 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import fetch from 'node-fetch'
+import axios from 'axios'
+
+
+global.owner = [
+  ['919614477116', '🔰—(••÷[ Š𝓊ḅᤂ𝖗𝓊 ]÷••)—💌', true],
+  ['919614477116', '🔰—(••÷[ Š𝓊ḅᤂ𝖗𝓊 ]÷••)—💌', true], 
+  [''] 
+] //Number of owners
+
+global.mods = ['919614477116','919614477116'] 
+global.prems = ['94767939688', '94767939688', '94767939688']
+global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.lolkeysapi = ['BrunoSobrino']
+
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  xteam: 'https://api.xteam.xyz', 
+  dzx: 'https://api.dhamzxploit.my.id',
+  lol: 'https://api.lolhuman.xyz',
+  violetics: 'https://violetics.pw',
+  neoxr: 'https://api.neoxr.my.id',
+  zenzapis: 'https://zenzapis.xyz',
+  akuari: 'https://api.akuari.my.id',
+  akuari2: 'https://apimu.my.id',
+  nrtm: 'https://fg-nrtm.ddns.net',
+  bg: 'http://bochil.ddns.net',
+  fgmods: 'https://api-fgmods.ddns.net'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
+  'https://api.neoxr.my.id': `${keysneoxr}`,	
+  'https://violetics.pw': 'beta',
+  'https://zenzapis.xyz': `${keysxxx}`, 
+  'https://api-fgmods.ddns.net': 'fg-dylux'
+}
+
+// Sticker WM
+global.botname = '📮 ___𝐋 𝐔 𝚻 𝚮 𝚵 𝚪 ___ 💌'
+global.premium = 'true'
+global.packname = '🎋💔𝐇𝐄𝐘 𝐓𝐇𝐄𝐑𝐄 𝐈𝐌ＳＵＢＡＲＵ🌷๑˃𝐀𝐒𝐈𝐀𝐒 𝐁𝐈𝐆𝐆𝐄𝐒𝐓 𝐒𝐓𝐈𝐂𝐊𝐄𝐑𝐒 𝐂𝐑𝐄𝐀𝐓𝐎𝐑🍂ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ' 
+global.author = '🕊+919614477116_ (ｉｎｓｔａ - lexx_lutheer_)' 
+global.igfg = '▢ Follow on Facebook\https://www.facebook.com/profile.php?id=61551915615888\n' 
+global.dygp = 'https://chat.whatsapp.com/KS5O3xRzVha6uJ8HFW8uWN'
+global.fgsc = 'https://github.com/mrluther-on' 
+global.fgyt = 'https://chat.whatsapp.com/KS5O3xRzVha6uJ8HFW8uWN'
+global.fgpyp = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
+global.fglog = 'WhatsApp Image 2024-11-06 at 19.23.36_1dd497ec.jpg' 
+
+
+global.wait = '* _Waiting Bro Uploading by 🔰ＳＵＢＡＲＵ💌*'
+global.rwait = '💌'
+global.dmoji = '🤣'
+global.done = '👍'
+global.error = '👎' 
+global.xmoji = '🫶' 
+
+global.multiplier = 69 
+global.maxwarn = '3' // máxima advertencias
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
